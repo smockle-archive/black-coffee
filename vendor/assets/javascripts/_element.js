c945aa9f72df97c2244d108d7cb0a4a778d6e2f1
Clay Miller <clay@smockle.com>
2014-05-17 00:51:43 -0400

    Add jshint options and comments.

--- vendor/assets/javascripts/_element.js
+++ vendor/assets/javascripts/_element.js
@@ -1,3 +1,26 @@
+/*jshint bitwise: true, curly: true, eqeqeq: true, forin: true, immed: true, indent: 4, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, quotmark: double, undef: true, unused: vars, strict: true, trailing: true, maxdepth: 3, devel: true, asi: true */
+/*global HTMLElement: true */
+
+//
+// ELEMENT
+//
+
+// Closest
+// Usage: element.closest(selector)
+this.Element && function (ElementPrototype) {
+    ElementPrototype.closest = 
+    function (selector) {
+        var node = this;
+        while (node) {
+            if (node == document) return undefined;
+            if (node.matches(selector)) return node; 
+            node = node.parentNode;
+        }
+    }
+} (Element.prototype);
+
+// Matches
+// Usage: element.matches(selector)
 this.Element && function (ElementPrototype) {
     ElementPrototype.matches = ElementPrototype.matches ||
     ElementPrototype.matchesSelector || 
@@ -8,17 +31,5 @@ this.Element && function (ElementPrototype) {
         var node = this, nodes = (node.parentNode || node.document).querySelectorAll(selector), i = -1;
         while (nodes[++i] && nodes[i] != node);
         return !!nodes[i];
-    }
-} (Element.prototype);
-
-this.Element && function (ElementPrototype) {
-    ElementPrototype.closest = 
-    function (selector) {
-        var node = this;
-        while (node) {
-            if (node == document) return undefined;
-            if (node.matches(selector)) return node; 
-            node = node.parentNode;
-        }
     }
 } (Element.prototype);
